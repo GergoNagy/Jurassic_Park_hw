12a0f6b3025be41f5710e6a0a7ca87d48a53518e
Gergo Nagy <guzmop@gmail.com>
2017-08-29 21:16:41 +0100

    mvp done

--- park.js
+++ park.js
@@ -1,5 +1,30 @@
+var Dinosaur = require("./dinosaur.js");
+
 var Park = function(){
   this.enclosure = [];
+}
+
+Park.prototype = {
+  addDino: function(dino){
+    this.enclosure.push(dino);
+  },
+  removeByTipe: function(type){
+    for( var i = this.enclosure.length - 1; i > -1; i--){
+      if (this.enclosure[i].type == type) {
+        this.enclosure.splice(i, 1);
+      }
+    }
+  },
+  offspringMoreThanTwo: function(number){
+    for( var i = this.enclosure.length - 1; i > -1; i--){
+      if (this.enclosure[i].offSpring <= number){
+        this.enclosure.splice(i, 1);
+      }
+    }
+  },
+  populationByYear: function(numberOfDino, year){
+    this.enclosure[0].offSpring;
+  }
 }
 
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -13,10 +13,39 @@ describe("Park", function(){
     park = new Park;
     dinosaur1 = new Dinosaur("Tyrannosaurus", 4);
     dinosaur2 = new Dinosaur("Velociraptor", 6);
-    dinosaur3 = new Dinosaur("Triceratops", 2);
+    dinosaur3 = new Dinosaur("Triceratops", 1);
   })
 
   it("hasSize", function(){
     assert.strictEqual(park.enclosure.length, 0);
   })
+
+  it("addDinoToThePark", function(){
+    park.addDino(dinosaur1);
+    assert.strictEqual(park.enclosure[0].type, "Tyrannosaurus");
+  })
+
+  it("Remove by type", function(){
+    park.addDino(dinosaur1);
+    park.addDino(dinosaur1);
+    park.addDino(dinosaur2);
+    park.removeByTipe("Tyrannosaurus");
+    assert.strictEqual(park.enclosure[0].type, "Velociraptor");
+  })
+
+  it("if offspring more than 2", function() {
+    park.addDino(dinosaur2);
+    park.addDino(dinosaur3);
+    park.offspringMoreThanTwo(2);
+    assert.strictEqual(park.enclosure.length, 1);
+  })
+
+  it("how many dino after a year", function(){
+    park.addDino(dinosaur2);
+    park.populationByYear(1,1);
+    console.log("",park);
+    console.log("after",park.enclosure[0].offSpring);
+    assert.strictEqual(park.populationByYear, 7);
+  })
+
 })
